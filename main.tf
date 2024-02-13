56c332113e94916e8a029d3c21a39e1937cdde22
Rafael García Álvarez <vattenbit@gmail.com>
2024-02-13 20:21:43 +0100

    Delete vnet and subnet

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ resource "azurerm_resource_group" "rg_we" {
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "net_we" {
-  name                = "netweu84b1"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg_we.location
-  resource_group_name = azurerm_resource_group.rg_we.name
-}
+#resource "azurerm_virtual_network" "net_we" {
+#  name                = "netweu84b1"
+#  address_space       = ["10.0.0.0/16"]
+#  location            = azurerm_resource_group.rg_we.location
+#  resource_group_name = azurerm_resource_group.rg_we.name
+#}
 
-resource "azurerm_subnet" "snt_we" {
-  name                 = "sntweu84b1"
-  resource_group_name  = azurerm_resource_group.rg_we.name
-  virtual_network_name = azurerm_virtual_network.net_we.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+#resource "azurerm_subnet" "snt_we" {
+#  name                 = "sntweu84b1"
+#  resource_group_name  = azurerm_resource_group.rg_we.name
+#  virtual_network_name = azurerm_virtual_network.net_we.name
+#  address_prefixes     = ["10.0.1.0/24"]
+#}
 
 #resource "azurerm_network_interface" "example" {
 #  name                = "example-nic"
